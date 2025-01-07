5ca55e0bc6cccc7f3b47527cfd8f7fa30e61a7e5
Dev <136987161+DimitriGlazov@users.noreply.github.com>
2025-01-07 17:08:19 +0530

    Update Veriquick_server_proto.py

--- Veriquick_server_proto.py
+++ Veriquick_server_proto.py
@@ -96,8 +96,8 @@ def generate_qr_code_with_metadata(files_metadata):
     return img
 
 # Main Streamlit App
-st.set_page_config(layout="wide", page_title="Veriquick", page_icon="✅")
-st.title('Veriquick')
+st.set_page_config(layout="wide", page_title="Veriquick✅", page_icon="✅")
+st.title('Veriquick✅')
 st.write(" Let's make verification paperless")
 
 
